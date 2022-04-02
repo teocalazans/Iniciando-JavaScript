c414c8a83514b73a9979cd8035a1e85581915fa2
Teocalazans <teocalazans20@gmail.com>
2022-04-02 14:32:00 -0300

    Estudando Funções em JavaScript

---
+++ Aula 5/style.js
@@ -0,0 +1,12 @@
+function funcao () {
+    console.log("Tudo certo na bahhia")
+}
+
+funcao ()
+
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem("tudo certo", "jovem!")

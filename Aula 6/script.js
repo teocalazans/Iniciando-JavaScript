7fa0abbc028070dacc4b2bafaf3a5609ae3a2158
Teocalazans <teocalazans20@gmail.com>
2022-04-02 16:05:27 -0300

    Um pouco mais de Função + Criação de uma calculadora em JS

---
+++ Aula 6/script.js
@@ -0,0 +1,28 @@
+// tipo de função 
+
+//função declarativa
+
+function aula6() {
+    console.log("Tudo certo na massa bahia")
+}
+
+aula6()
+
+// expressão de função
+//com nomeação
+var func = function func() {
+    console.log("Tudo mais que certo na nossa bahia")
+}
+func()
+
+//sem nomeação 
+var func = function() {
+    console.log("Tudo muito mais que certo na nossa bahia")
+}
+func()
+
+//Arrow-function
+var sup = () => {
+    console.log("Tudo bastante muito mais que certo na nossa linda bahia")
+}
+sup()
